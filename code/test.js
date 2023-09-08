5597abc3aa49ff8e0141a6c65e904879cb67ee8c
Gourav Sharma <102802282+gouravpvt1408@users.noreply.github.com>
2023-09-08 14:06:45 +0000

    Data Types

---
+++ code/test.js
@@ -0,0 +1,33 @@
+console.log("hey buddy")
+
+
+
+const name = "viraj Sharma"
+let email = "jpg@gmail.com"
+var passwords = "noponopo"
+city = "jaipur"
+let state;
+
+console.table([ name,email,passwords,city,state])
+
+// theorypart
+// const is used for the when you are clear that you not gonna change the values or not gonna be changed;
+// var datatype is no longer  used in developers world because it will create problems while makeing int the scope and block;
+// console.table  is used to see every console log in the into table format;
+
+
+
+                               // primitive Data types
+// Number => 2 to thr power of 53
+// bigint => it is used for the very big number eg. giant apps ,stock market etc.
+// string => " " everything comes under the double cooma's  
+// boolean => it will onlt ans YES/NO  
+// undefined => when you not asgined the values that is known as undefined
+//  null => it is standalone Value.
+
+
+                    //   USE STRICT  //
+               
+     // it is shows that in code you can only use the new type coding //
+
+    //  to get type of the value we use "console.log (typeof)"       // 
