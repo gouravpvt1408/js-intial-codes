f9f38f37dfd3d894b7bfc89ae1f8df65800fdb70
Gourav Sharma <102802282+gouravpvt1408@users.noreply.github.com>
2023-09-08 17:22:06 +0000

    Oprations

---
+++ code/oprations.js
@@ -0,0 +1,21 @@
+                //  Oprations //
+
+
+    // console.log(2+2)    
+    // console.log(2-2)
+    // console.log(2*2)
+    // console.log(2**2)
+    // console.log(2/2)   
+    
+    
+    // console.log("2"+2)  
+    // console.log(2+"2")  
+    // console.log(2+2+"2")  
+    // console.log("2"+"2")  
+    // console.log((4+2)*6+"2")  
+
+
+    let gameCounter = 50
+    ++gameCounter;
+
+    console.log(gameCounter)
